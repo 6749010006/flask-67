9cae4f7639d5bd00a1639907b7e0c51cff257b92
Chatchawal <6749010006@phuketvc.ac.th>
2024-07-07 16:28:33 +0700

    _

--- app.py
+++ app.py
@@ -1,31 +1,122 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, redirect,request, session
+import mysql.connector
 
 app = Flask(__name__)
 
+# For form
+app.config['SECRET_KEY'] = 'wb32'
+
+#For DB
+db_host = "localhost"
+db_user = "root"
+db_pass = ""
+db_name = "flower"
+
 @app.route("/home")
 def home():
-    #return "<h1>Home</h1>"
+    # return "<h1>Home</h1>"
     name = "Lisa"
     age = 27
-    my_dict = {"name":"Lisa","age":27}
-    return render_template("home.html", name=name, age=age,my_dict=my_dict)
+    my_dict = {"name": "Lisa", "age": 27}
+    return render_template("home.html", name=name, age=age,
+    my_dict=my_dict)
 
-@app.route("/create",methods=["GET"])
+@app.route("/create", methods=["GET"])
 def create():
     return render_template("create.html")
 
-@app.route("/store",methods=["POST"])
+@app.route("/store", methods=["POST"])
 def store():
+    # from flask import Flask, render_template, request
     if request.method == "POST":
         flower_name = request.form['flower_name']
         lat_num = request.form['lat_num']
-        long_num= request.form['long_num']
-        place= request.form['place']
+        long_num = request.form['long_num']
+        place = request.form['place']
         detail = request.form['detail']
-        print(flower_name,lat_num,long_num,place,detail)
-    return redirect('/home')
+        print(flower_name, lat_num, long_num, place, detail)
+        
+        # pip install mysql-connector-python
+        # forom flask import Flask, render_template, request, redirect, session
+        # import mysql.connector
+        # Connec Database
+        my_db = mysql.connector.connect(
+            host = db_host,
+            user = db_user,
+            password = db_pass,
+            db = db_name
+            )
+        my_cursor = my_db.cursor(dictionary=True)
+        sql = "INSERT INTO flowers(flower_name, lat_num, long_num, place, detail) VALUES(%s, %s, %s, %s,%s)"
+        val = (flower_name, lat_num, long_num, place, detail)
+        my_cursor.execute(sql, val)
+        my_db.commit()
 
-if __name__=="__main__":
+        session['alert_status'] = "success"
+        session['alert_message'] = "already Created!"
+        return redirect('/')
+    else:
+        session['alert_status'] = "fill"
+        session['alert_message'] = "something went wrong!"
+        return redirect('/')
+    
+@app.route("/")
+def index():
+    my_db = mysql.connector.connect(
+            host = db_host,
+            user = db_user,
+            password = db_pass,
+            db = db_name
+    )
+    my_cursor = my_db.cursor(dictionary=True)
+    sql = "SELECT * FROM flowers"
+    my_cursor.execute(sql)
+    results= my_cursor.fetchall()
+    #show Message
+    if 'alert_status' in session and 'alert_message' in session:
+        alert_message = {
+            'status' : session['alert_status'],
+            'message' :session['alert_message']
+        }
+        del session ['alert_status']
+        del session['alert_message']
+    else :
+        alert_message = {
+            'status' : None,
+            'message' : None
+        }
+    
+    return render_template("index.html",alert_message=alert_message,results=results)
+        
+@app.route("/edit/<flower_id>",methods=['GET'])
+def edit(flower_id):
+    my_db = mysql.connector.connect(
+        host = db_host,
+        user = db_user,
+        password = db_pass,
+        db = db_name
+    )
+    my_cursor = my_db.cursor(dictionary=True)
+    sql = "SELECT * FROM flowers WHERE id = " + flower_id
+    my_cursor.execute(sql)
+    results= my_cursor.fetchall() 
+    return render_template("edit.html", results = results)
+
+@app.route("/update/<flower_id>", methods=["POST"])
+def update(flower_id):
+    if request.method == "POST":
+        flower_name = request.form['flower_name']
+        lat_num = request.form['lat_num']
+        long_num = request.form['long_num']
+        place = request.form['place']
+        detail = request.form['detail']
+        print(flower_name, lat_num, long_num, place, detail)
+    return redirect("/")
+
+
+
+
+
+if __name__ == "__main__":
     app.run(debug=True)
-    #app.run()
-
+    # opp.run()
